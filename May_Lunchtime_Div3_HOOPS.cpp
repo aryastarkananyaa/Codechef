e0a2a1dc0f3686ff2343efe4ca6bcaceaa265486
Ananya Arya <49473739+aryastarkananyaa@users.noreply.github.com>
2021-05-31 23:31:47 +0530

    Create May_Lunchtime_Div3_HOOPS.cpp
    https://www.codechef.com/LTIME96C/problems/HOOPS

---
+++ May_Lunchtime_Div3_HOOPS.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+long long t;
+cin>>t;
+while(t--){
+long long n;
+cin>>n;
+if(n==1) cout<<"1"<<"\n";
+else{
+cout<<(n/2)+1<<"\n";
+}
+}
+return 0;
+}
